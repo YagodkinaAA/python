8f0bea906d2e0bfd328e52e22fbe991d1c0a2167
YagodkinaAA <anastasiayagodckina@yandex.ru>
2023-03-03 11:53:19 +0300

    рассортировала файлы

---
+++ src/модуль 15.py
@@ -0,0 +1,23 @@
+from random import *
+
+cn = int(input('введите границу поиска:\n'))
+num = randrange(n)
+print(num)
+value = -1
+left = 1
+right = n
+col=0
+while value != num:
+    col+=1
+    value = int(input('как вы думаете какое число я загадала?:\n'))
+    middle = (left + right) // 2
+    if middle == num:
+        print('вы угадали!\n')
+        print('вам понадобилось', col, 'попыток')
+    else:
+        if middle < num:
+            print('Ваше число ')
+            left = middle + 1
+    if middle > num:
+        right = middle - 1
+print(value)
