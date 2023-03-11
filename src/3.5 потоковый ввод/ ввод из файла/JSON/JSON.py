53e766f7b6be41a053af157b113de29ea3a9d96d
YagodkinaAA <anastasiayagodckina@yandex.ru>
2023-03-11 10:40:56 +0300

    JSON файл

---
+++ src/3.5 потоковый ввод/ ввод из файла/JSON/JSON.py
@@ -0,0 +1,16 @@
+import json
+from sys import stdin
+
+slov = []
+print('введите строки вида ключ == значение')
+for line in stdin:
+    slov.append(line.rstrip('\n').split(' '))
+#print(slov)
+with open("data.json", encoding="UTF-8") as file1:
+    value = json.load(file1)
+#print(value)
+for f in slov:
+    value[f[0]] = f[2]
+#print(value)
+with open("data1.json", 'w', encoding="UTF-8") as file1:
+    json.dump(value, file1, ensure_ascii=False, indent=2)
