27ede0e3996055ebf2bb8a307db1ac44d5425353
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-03-17 14:08:20 +0300

    Рекурсивная сумма

--- src/рекрусивные функции/2.py
+++ src/рекрусивные функции/2.py
@@ -1,28 +1,10 @@
-summa = 0
-
-
 def recursive_digit_sum(num):
-    last_num = num % 10
-    global summa
-    summa += last_num
-    num //= 10
-    if num != 0:
-        recursive_digit_sum(num)
-    return summa
-
-#почему надо делить на 2(не совсем понимаю принцип работы)
-def digit_sum(num):
-    last_num = num % 10
-    if num==0:
+    if num == 0:
         return 0
     else:
-        return round((last_num+recursive_digit_sum(num//10))/2)
-
-
-
-
+        # (a // b, a % b)
+        (d, r) = divmod(num, 10)
+        return recursive_digit_sum(d) + r
 
 
 print(recursive_digit_sum(123))
-print(summa)
-print(digit_sum(123))
