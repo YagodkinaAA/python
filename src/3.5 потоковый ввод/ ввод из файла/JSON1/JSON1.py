2af144a135b9b0e2e2e5457514cdf7ccd5335f7b
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-03-17 13:18:44 +0300

    Исправил алгоритм

--- src/3.5 потоковый ввод/ ввод из файла/JSON1/JSON1.py
+++ src/3.5 потоковый ввод/ ввод из файла/JSON1/JSON1.py
@@ -1,18 +1,22 @@
 import json
 
-zap=dict()
 with open("users.json", encoding="UTF-8") as file1:
     value = json.load(file1)
 print(value)
+
 with open("updates.json", encoding="UTF-8") as file2:
     new = json.load(file2)
 print(new)
+
+zap = dict()
 for i in range(len(value)):
-    zap[value[i]['name']]=value[i]
-print(zap)
+    zap[value[i]['name']] = value[i]
+
 for j in range(len(zap)):
     if new[j]['name'] in zap:
-        zap[value[j]['name']]=new[j]
+        zap[value[j]['name']].update(new[j])
+        zap[value[j]['name']].pop("name")
 print(zap)
-with open("users1.json", 'w',encoding="UTF-8") as file3:
+
+with open("users1.json", 'w', encoding="UTF-8") as file3:
     json.dump(zap, file3, ensure_ascii=False, indent=2)
