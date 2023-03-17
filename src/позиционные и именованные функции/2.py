cea0eb8ee5a195fb6387b413ebbadd4e9744db52
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-03-17 13:47:43 +0300

    Возня с tuple и матрицами

--- src/позиционные и именованные функции/2.py
+++ src/позиционные и именованные функции/2.py
@@ -1,10 +1,16 @@
-def make_matrix(size, value=0):
+def make_matrix(*size, value=0):
     p = []
-    k = []
-    for _ in range(size[0]):
-        p.append(value)
-    k = [p for _ in range(size[1])]
-    return k
+    if type(size[0]) == int:
+        rows = size[0]
+        cols = size[0]
+    else:
+        rows = size[0][1]
+        cols = size[0][0]
+
+    for _ in range(rows):
+        p.append([value for _ in range(cols)])
+    return p
+
 
 print(*make_matrix((4, 2), value=1), sep='\n')
-#print(make_matrix(3))
+print(*make_matrix(3), sep='\n')
