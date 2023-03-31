c729455458cea15a3d5a042fc4c365e1eafa58fb
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-03-31 13:24:18 +0300

    Ещё лучше написал!

--- src/рекрусивные функции/3.py
+++ src/рекрусивные функции/3.py
@@ -2,8 +2,7 @@ def make_equation(*num):
     if len(num) == 1:
         return num[0]
     else:
-        (*first, last) = num
-        return f'({make_equation(*first)}) * x + {last}'
+        return f'({make_equation(*num[:-1])}) * x + {num[-1]}'
 
 
 print(make_equation(3, 1, 5, 3))
