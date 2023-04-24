384c429c4a2f1f26fededbc9b03f525793a2ad29
YagodkinaAA <anastasiayagodckina@yandex.ru>
2023-04-24 21:39:17 +0300

    Задачка на волшебные методы

---
+++ src/ООП/5.2.3.py
@@ -0,0 +1,51 @@
+from math import *
+
+
+class Point:
+
+    def __init__(self, a, b):
+        self.x = a
+        self.y = b
+        self.dlina = 0
+
+    def move(self, c, d):
+        self.x = self.x + c
+        self.y = self.y + d
+
+    def length(self, new_point):
+        self.dlina = sqrt((new_point.x - self.x) ** 2 + (new_point.y - self.y) ** 2)
+        return round(self.dlina, 2)
+
+
+class PatchedPoint(Point):
+    def __init__(self, *coord):
+        if len(coord) == 1:
+            super().__init__(a=coord[0][0], b=coord[0][1])
+        elif len(coord) == 0:
+            super().__init__(a=0, b=0)
+        elif len(coord) == 2:
+            super().__init__(a=coord[0], b=coord[1])
+
+    def __add__(self, *other):
+        point = PatchedPoint()
+        point.x = self.x + other[0][0]
+        point.y = self.y + other[0][1]
+        return point
+
+    def __iadd__(self, *other):
+        self.x += other[0][0]
+        self.y += other[0][1]
+        return self
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
+
+
+point = PatchedPoint()
+print(point)
+new_point = point + (2, -3)
+print(point, new_point, point is new_point)
+
+first_point = second_point = PatchedPoint((2, -7))
+first_point += (7, 3)
+print(first_point, second_point, first_point is second_point)
