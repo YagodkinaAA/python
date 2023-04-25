f2501a4cd49d51590b1b6587884cf853c2d14ae9
YagodkinaAA <anastasiayagodckina@yandex.ru>
2023-04-25 09:28:40 +0300

    рекрусия, два метода решения

--- src/рекрусивные функции/3.py
+++ src/рекрусивные функции/3.py
@@ -3,6 +3,12 @@ def make_equation(*num):
         return num[0]
     else:
         return f'({make_equation(*num[:-1])}) * x + {num[-1]}'
-
+'''def make_equation(*num):
+    if len(num) == 1:
+        return num[0]
+    else:
+        (*first, last) = num
+        return f'({make_equation(*first)}) * x + {last}'
+'''
 
 print(make_equation(3, 1, 5, 3))
