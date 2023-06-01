4a28a8517049c4a426f11045cc3b908ecff76882
YagodkinaAA <anastasiayagodckina@yandex.ru>
2023-06-01 11:21:35 +0300

    Reformat

--- src/plot12.py
+++ src/plot12.py
@@ -1,21 +1,20 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-#part1
+# part1
 fig = plt.figure(figsize=(7, 4))
 ax = fig.add_subplot()
-vals = [10, 40, 23, 30, 7]
+values = [10, 40, 23, 30, 7]
 labels = ['Toyota', 'BMW', 'Lexus', 'Audi', 'Lada']
 exp = (0.1, 0.2, 0, 0, 0)
-ax.pie(vals, labels=labels, autopct='%.2f', explode=exp, shadow=True)
+ax.pie(values, labels=labels, autopct='%.2f', explode=exp, shadow=True)
 ax.grid()
 
-#part1
+# part2
 fig1 = plt.figure(figsize=(7, 4))
 ax1 = fig1.add_subplot()
-vals = [10, 40, 23, 30, 7]
+values = [10, 40, 23, 30, 7]
 labels = ['Toyota', 'BMW', 'Lexus', 'Audi', 'Lada']
 exp = (0.1, 0.2, 0, 0, 0)
-ax1.pie(vals, labels=labels, autopct='%.2f', wedgeprops=dict(width=0.5), shadow=True)
+ax1.pie(values, labels=labels, autopct='%.2f', wedgeprops=dict(width=0.5), shadow=True)
 ax1.grid()
 plt.show()
