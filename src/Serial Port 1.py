67cd43969a9998a4ff092b64343530de27c0fb97
YagodkinaAA <anastasiayagodckina@yandex.ru>
2023-07-25 19:09:09 +0300

    Reformat

--- src/Serial Port 1.py
+++ src/Serial Port 1.py
@@ -1,7 +1,17 @@
 import serial
+from struct import *
 
-ser = serial.Serial(port="COM5", baudrate=9600, bytesize=8, timeout=1, stopbits=serial.STOPBITS_ONE)
-ser.write("Hello!".encode('Ascii'))
-ser.write("Прыгни вверх".encode('UTF-8'))
-print(ser.readline().decode('UTF-8'))
+ser = serial.Serial(port="COM5", baudrate=57600, bytesize=8, timeout=0.01, parity=serial.PARITY_ODD,
+                    stopbits=serial.STOPBITS_ONE)
+
+id = 0
+x = 20000
+y = 0
+g = 0
+c = 50
+ser.write(pack('<ccchhhh', b'\x5a', id.to_bytes(), b'\x08', x, y, g, c))
+#ser.write("Прыгни вверх".encode('UTF-8'))
+print(ser.readline().hex())
+# print(ser.read(size=10).decode('UTF-8'))
+
 ser.close()
