90be3344fe02b8b926b356d757f97f5049467f0e
YagodkinaAA <anastasiayagodckina@yandex.ru>
2023-12-22 14:54:24 +0300

    2 привода через классы

--- src/Serial Port 5.py
+++ src/Serial Port 5.py
@@ -16,13 +16,26 @@ g = 0
 c = 50
 
 
+class Controller:
+    def __init__(self):
+        self.target = 0
+        self.now = 0
+        self.speed = 0
+
+    def put(self, read, target):
+        self.target = target
+        self.speed = (int(target - read // 100)) // 10 * 500
+        self.speed = int(min(abs(self.speed), 10_000) * numpy.sign(self.speed))
+        self.now = read // 100
+
+
 def data_check():
     global rotate_percent
     global lift_percent
     while True:
-        if keyboard.is_pressed("right arrow"):
+        if keyboard.is_pressed("left arrow"):
             rotate_percent += 10
-        elif keyboard.is_pressed("left arrow"):
+        elif keyboard.is_pressed("right arrow"):
             rotate_percent -= 10
         elif keyboard.is_pressed("down arrow"):
             lift_percent += 10
@@ -34,19 +47,20 @@ def data_check():
 i = 0
 thr = threading.Thread(target=data_check, daemon=True)
 thr.start()
-
+rotate = Controller()
+catch = Controller()
 while i != 5000:
+    x = rotate.speed
+    y = catch.speed
     ser.write(struct.pack('<ccchhhh', b'\x5a', i.to_bytes(), b'\x08', x, y, g, c))
     time.sleep(1)
     read = struct.unpack('<cccih', ser.read(9))
 
-    x = (int(rotate_percent - read[3] // 100)) // 10 * 500
-    x = int(min(abs(x), 10_000) * numpy.sign(x))
+    rotate.put(read[3], rotate_percent)
+    print(f'Поворот: {rotate.__dict__}')
 
-    y = (int(lift_percent - read[4] // 100)) // 10 * 500
-    y = int(min(abs(y), 10_000) * numpy.sign(y))
-    # print(read)
-    print(f'Поворот: target = {rotate_percent} %; now = {read[3] // 100} %; speed = {x}')
-    print(f'Подъем: target = {lift_percent} %; now = {read[4] // 100} %; speed = {y}')
+    catch.put(read[4], lift_percent)
+    print(f'Подъем: {catch.__dict__}')
+
     i = i + 1
 ser.close()
