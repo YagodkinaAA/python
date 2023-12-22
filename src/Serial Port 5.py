9515d21affadd42367469a5d55eeff0890d9bdce
YagodkinaAA <anastasiayagodckina@yandex.ru>
2023-12-22 12:51:53 +0300

    Работает на 2 приводах

--- src/Serial Port 5.py
+++ src/Serial Port 5.py
@@ -9,6 +9,7 @@ ser = serial.Serial(port="COM10", baudrate=57600, bytesize=8, timeout=0.01, pari
                     stopbits=serial.STOPBITS_ONE)
 
 rotate_percent = 0
+lift_percent = 0
 x = 0
 y = 0
 g = 0
@@ -17,11 +18,16 @@ c = 50
 
 def data_check():
     global rotate_percent
+    global lift_percent
     while True:
         if keyboard.is_pressed("right arrow"):
             rotate_percent += 10
         elif keyboard.is_pressed("left arrow"):
             rotate_percent -= 10
+        elif keyboard.is_pressed("down arrow"):
+            lift_percent += 10
+        elif keyboard.is_pressed("up arrow"):
+            lift_percent -= 10
         time.sleep(0.2)
 
 
@@ -36,6 +42,11 @@ while i != 5000:
 
     x = (int(rotate_percent - read[3] // 100)) // 10 * 500
     x = int(min(abs(x), 10_000) * numpy.sign(x))
-    print(f'target = {rotate_percent} %; now = {read[3] // 100} %; speed = {x}')
+
+    y = (int(lift_percent - read[4] // 100)) // 10 * 500
+    y = int(min(abs(y), 10_000) * numpy.sign(y))
+    # print(read)
+    print(f'Поворот: target = {rotate_percent} %; now = {read[3] // 100} %; speed = {x}')
+    print(f'Подъем: target = {lift_percent} %; now = {read[4] // 100} %; speed = {y}')
     i = i + 1
 ser.close()
